098573b5b46c57b6f018ea927f20633a7d3d09f2
jthua <jthua89@gmail.com>
2024-05-11 11:54:35 +0800

    添加项目文件。

---
+++ ActivityInteractionCoefficient.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace AlloyAct_Pro
+{
+    partial class ActivityInteractionCoefficient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(dataGridView1);
+            splitContainer1.Size = new Size(914, 676);
+            splitContainer1.SplitterDistance = 198;
+            splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 21.3058414F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 78.69416F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 324F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 283F));
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(914, 198);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(914, 474);
+            dataGridView1.TabIndex = 0;
+            // 
+            // ActivityInteractionCoefficient
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 676);
+            Controls.Add(splitContainer1);
+            Name = "ActivityInteractionCoefficient";
+            Text = "Activity Interaction Coefficients";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AlloyAct_Pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            Activity = new Button();
+            ActivityCoeff = new Button();
+            InteractionCoeff = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.4F));
+            tableLayoutPanel1.Controls.Add(Activity, 0, 1);
+            tableLayoutPanel1.Controls.Add(ActivityCoeff, 1, 1);
+            tableLayoutPanel1.Controls.Add(InteractionCoeff, 2, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.4F));
+            tableLayoutPanel1.Size = new Size(777, 637);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Activity
+            // 
+            Activity.Dock = DockStyle.Fill;
+            Activity.Font = new Font("楷体", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            Activity.Location = new Point(3, 215);
+            Activity.Name = "Activity";
+            Activity.Size = new Size(252, 206);
+            Activity.TabIndex = 0;
+            Activity.Text = "活度";
+            Activity.UseVisualStyleBackColor = true;
+            // 
+            // ActivityCoeff
+            // 
+            ActivityCoeff.Dock = DockStyle.Fill;
+            ActivityCoeff.Font = new Font("楷体", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            ActivityCoeff.Location = new Point(261, 215);
+            ActivityCoeff.Name = "ActivityCoeff";
+            ActivityCoeff.Size = new Size(252, 206);
+            ActivityCoeff.TabIndex = 1;
+            ActivityCoeff.Text = "活度系数";
+            ActivityCoeff.UseVisualStyleBackColor = true;
+            // 
+            // InteractionCoeff
+            // 
+            InteractionCoeff.Dock = DockStyle.Fill;
+            InteractionCoeff.Font = new Font("楷体", 16.5F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            InteractionCoeff.Location = new Point(519, 215);
+            InteractionCoeff.Name = "InteractionCoeff";
+            InteractionCoeff.Size = new Size(255, 206);
+            InteractionCoeff.TabIndex = 2;
+            InteractionCoeff.Text = "活度相互作用系数";
+            InteractionCoeff.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(777, 637);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "AlloymeltActivity Pro";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button Activity;
+        private Button ActivityCoeff;
+        private Button InteractionCoeff;
+    }
+}

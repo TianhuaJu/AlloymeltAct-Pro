1f64933ff41defaa6e1ceeafc98eddec48ddeae2
jthua <jthua89@gmail.com>
2024-05-11 12:21:10 +0800

    添加背景图

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,24 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
-            Activity = new Button();
             ActivityCoeff = new Button();
+            Activity = new Button();
             InteractionCoeff = new Button();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
+            tableLayoutPanel1.BackColor = Color.FromArgb(128, 255, 255);
+            tableLayoutPanel1.BackgroundImage = Properties.Resources.R;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetDouble;
             tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.4F));
-            tableLayoutPanel1.Controls.Add(Activity, 0, 1);
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
             tableLayoutPanel1.Controls.Add(ActivityCoeff, 1, 1);
-            tableLayoutPanel1.Controls.Add(InteractionCoeff, 2, 1);
+            tableLayoutPanel1.Controls.Add(Activity, 1, 0);
+            tableLayoutPanel1.Controls.Add(InteractionCoeff, 1, 2);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -54,38 +57,41 @@
             tableLayoutPanel1.Size = new Size(777, 637);
             tableLayoutPanel1.TabIndex = 0;
             // 
-            // Activity
-            // 
-            Activity.Dock = DockStyle.Fill;
-            Activity.Font = new Font("楷体", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            Activity.Location = new Point(3, 215);
-            Activity.Name = "Activity";
-            Activity.Size = new Size(252, 206);
-            Activity.TabIndex = 0;
-            Activity.Text = "活度";
-            Activity.UseVisualStyleBackColor = true;
-            // 
             // ActivityCoeff
             // 
+            ActivityCoeff.BackColor = Color.Cyan;
             ActivityCoeff.Dock = DockStyle.Fill;
             ActivityCoeff.Font = new Font("楷体", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            ActivityCoeff.Location = new Point(261, 215);
+            ActivityCoeff.Location = new Point(238, 217);
             ActivityCoeff.Name = "ActivityCoeff";
-            ActivityCoeff.Size = new Size(252, 206);
+            ActivityCoeff.Size = new Size(300, 202);
             ActivityCoeff.TabIndex = 1;
             ActivityCoeff.Text = "活度系数";
-            ActivityCoeff.UseVisualStyleBackColor = true;
+            ActivityCoeff.UseVisualStyleBackColor = false;
+            // 
+            // Activity
+            // 
+            Activity.BackColor = Color.Cyan;
+            Activity.Dock = DockStyle.Fill;
+            Activity.Font = new Font("楷体", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            Activity.Location = new Point(238, 6);
+            Activity.Name = "Activity";
+            Activity.Size = new Size(300, 202);
+            Activity.TabIndex = 3;
+            Activity.Text = "活度";
+            Activity.UseVisualStyleBackColor = false;
             // 
             // InteractionCoeff
             // 
+            InteractionCoeff.BackColor = Color.Cyan;
             InteractionCoeff.Dock = DockStyle.Fill;
-            InteractionCoeff.Font = new Font("楷体", 16.5F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            InteractionCoeff.Location = new Point(519, 215);
+            InteractionCoeff.Font = new Font("楷体", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            InteractionCoeff.Location = new Point(238, 428);
             InteractionCoeff.Name = "InteractionCoeff";
-            InteractionCoeff.Size = new Size(255, 206);
+            InteractionCoeff.Size = new Size(300, 203);
             InteractionCoeff.TabIndex = 2;
             InteractionCoeff.Text = "活度相互作用系数";
-            InteractionCoeff.UseVisualStyleBackColor = true;
+            InteractionCoeff.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -93,6 +99,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(777, 637);
             Controls.Add(tableLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
             Text = "AlloymeltActivity Pro";
             tableLayoutPanel1.ResumeLayout(false);
@@ -102,8 +109,8 @@
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private Button Activity;
         private Button ActivityCoeff;
         private Button InteractionCoeff;
+        private Button Activity;
     }
 }
